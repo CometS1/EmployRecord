ae58ae717343ba8c7d3f0bc0754f99ccdeba3b06
Shawn <sppang1@yahoo.com>
2019-02-24 10:07:12 -0500

    added new activities for each page

---
+++ app/src/main/java/ca/senecacollege/employrecord/ViewJob.java
@@ -0,0 +1,13 @@
+package ca.senecacollege.employrecord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewJob extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_job);
+    }
+}
