70b6b84b8fb44e807051874316757a4f668a8f35
Jeff <clee465@gmail.com>
2019-02-25 23:08:49 -0500

    re-enable db handler under registration activity

--- app/src/main/java/ca/senecacollege/employrecord/RegistrationActivity.java
+++ app/src/main/java/ca/senecacollege/employrecord/RegistrationActivity.java
@@ -4,10 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import ca.senecacollege.employrecord.DatabaseHelper.MyDBHandler;
+import ca.senecacollege.employrecord.DatabaseHelper.User;
 
 //import ca.senecacollege.employrecord.DatabaseHelper.MyDBHandler;
 
@@ -48,7 +53,7 @@ public class RegistrationActivity extends AppCompatActivity {
     }
 
     public void loadUser(View view) {
-        /*
+
         Log.i(TAG, "--> Start loadUser");
         MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
 
@@ -61,11 +66,11 @@ public class RegistrationActivity extends AppCompatActivity {
         email.setText("");
         pwd.setText("");
         pwdConfirm.setText("");
-        */
+
     }
 
     public void addUser(View view) {
-        /*
+
         Log.i(TAG, "--> Start addUser");
         MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
 
@@ -80,7 +85,7 @@ public class RegistrationActivity extends AppCompatActivity {
 
         dbHandler.addUserHandler(user);
         Toast.makeText(RegistrationActivity.this, "Registered! Load user!", Toast.LENGTH_LONG).show();
-        */
+
     }
 
 
