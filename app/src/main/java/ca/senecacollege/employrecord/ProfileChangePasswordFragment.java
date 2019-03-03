167e4b52997b6b90697bfbe8ed46cba40c0c9a94
Jeff <clee465@gmail.com>
2019-03-03 12:09:57 -0500

    init commit for profile fragment

--- app/src/main/java/ca/senecacollege/employrecord/ProfileChangePasswordFragment.java
+++ app/src/main/java/ca/senecacollege/employrecord/ProfileChangePasswordFragment.java
@@ -1,6 +1,5 @@
 package ca.senecacollege.employrecord;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,38 +8,32 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.Toast;
 
+public class ProfileChangePasswordFragment extends Fragment {
 
-public class ProfileFragment extends Fragment {
-
-
-    public ProfileFragment() {
+    public ProfileChangePasswordFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_profile_change_password, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        Button changePasswordFragment = (Button)view.findViewById(R.id.btn_change_pwd);
-        changePasswordFragment.setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.buttonTmp).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                    FragmentTransaction fragmentTransaction =getChildFragmentManager().beginTransaction();
-                    fragmentTransaction.replace(R.id.fragment_profile_change_password,new ProfileChangePasswordFragment());
-                    fragmentTransaction.commit();
+                Toast.makeText(getActivity(), "You are inside Notifications Fragment", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 
 }
