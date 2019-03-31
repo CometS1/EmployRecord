32c55fb61907cacf41f85bb5bcf48b7e060b9c18
Suzanna F <suzafung@users.noreply.github.com>
2019-03-31 10:49:34 -0400

    code documentation for Registration Activity

--- app/src/main/java/ca/senecacollege/employrecord/RegistrationActivity.java
+++ app/src/main/java/ca/senecacollege/employrecord/RegistrationActivity.java
@@ -1,7 +1,6 @@
 package ca.senecacollege.employrecord;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -38,15 +37,12 @@ public class RegistrationActivity extends AppCompatActivity {
     Button btnCancel;
     TextView resultView;
 
-    public static Intent newIntent(Context packageContext) {
-        Intent intent = new Intent(packageContext, RegistrationActivity.class);
-        return intent;
-    }
-
+    // Returns a new Database Handler
     private MyDBHandler dbHandler() {
         return new MyDBHandler(this, null, null, 1);
     }
 
+    // When the Registration Activity is opened/started, these actions are executed
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,6 +59,7 @@ public class RegistrationActivity extends AppCompatActivity {
         btnCancel = findViewById(R.id.btnCancel);
         resultView = findViewById(R.id.resultView);
 
+        // When clicking Register button, these actions execute
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -79,16 +76,20 @@ public class RegistrationActivity extends AppCompatActivity {
             }
         });
 
+        // When clicking Cancel button, these actions execute
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Request to start the Login Activity
                 Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
                 RegistrationActivity.this.startActivity(intent);
+
+                // Finish the Registration Activity
                 finish();
             }
         });
 
-        // TODO: below is temp fix for collapsing the keyboard, need to find another way
+        // Methods to collapse the keyboard when clicking out of a text box (screen area)
         fname.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
@@ -137,23 +138,26 @@ public class RegistrationActivity extends AppCompatActivity {
                 }
             }
         });
-        // TODO: above this line is temp fix
     }
 
+    // Returns TRUE if there is an email and it's in the correct format, otherwise FALSE
     private boolean isEmail(EditText text) {
         CharSequence email = text.getText().toString();
         return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
     }
 
+    // Returns TRUE if input text is blank, otherwise FALSE
     private boolean isEmpty(EditText text) {
         CharSequence str = text.getText().toString();
         return TextUtils.isEmpty(str);
     }
 
+    // Returns TRUE if password inputs match, otherwise FALSE
     private boolean isPasswordMatch(EditText password, EditText confirmPassword) {
         return password.getText().toString().equals(confirmPassword.getText().toString());
     }
 
+    // Returns TRUE if the password is valid, otherwise FALSE
     private boolean isValidPassword(EditText password) {
         Matcher matcher;
         //Minimum five characters, at least one letter, one number and one special character
@@ -163,6 +167,7 @@ public class RegistrationActivity extends AppCompatActivity {
         return matcher.matches();
     }
 
+    // Validate the data input by the user
     private boolean validateDataEntered() {
         boolean validFname = false;
         boolean validLname = false;
@@ -174,23 +179,30 @@ public class RegistrationActivity extends AppCompatActivity {
         boolean validConfirmPwd = false;
         boolean validPwdMatch = false;
 
+        // Log comment
         Log.e(TAG, "--> Start validateDataEntered");
 
+        // Set an error message if first name is blank
         if (isEmpty(fname)) {
             fname.setError("First name is required");
         } else {
             validFname = true;
         }
 
+        // Set an error message if last name is blank
         if (isEmpty(lname)) {
             lname.setError("Last name is required");
         } else {
             validLname = true;
         }
+
+        // Set an error message if username is blank
         if (isEmpty(username)) {
             username.setError("Username is required");
         } else {
             validUsername = true;
+
+            // Set an error message if username already exists in database
             if (dbHandler().isUser(username.getText().toString())) {
                 username.setError("Username already exists!");
             } else {
@@ -198,16 +210,20 @@ public class RegistrationActivity extends AppCompatActivity {
             }
         }
 
+        // Set an error message if email is blank
         if (!isEmail(email) || isEmpty(email)) {
             email.setError("Valid email is required");
         } else {
             validEmail = true;
         }
 
+        // Set an error message if password is blank
         if (isEmpty(pwd)) {
             pwd.setError("Password is required");
         } else {
             validPwd = true;
+
+            // Set an error message if password is invalid
             if (!isValidPassword(pwd)) {
                 pwd.setError("Password must have minimum 5 characters, at least 1 letter, 1 number and 1 special character");
             } else {
@@ -215,45 +231,54 @@ public class RegistrationActivity extends AppCompatActivity {
             }
         }
 
+        // Set an error message if password is blank
         if (isEmpty(pwdConfirm)) {
             pwdConfirm.setError("Confirm password is required");
         } else {
             validConfirmPwd = true;
         }
 
+        // Set an error message if password does not match
         if (!isPasswordMatch(pwd, pwdConfirm)) {
             pwdConfirm.setError("Your passwords must match");
         } else {
             validPwdMatch = true;
         }
 
+        // Returns TRUE if all data is valid
         return (validFname && validLname && validUsername && validUser && validEmail && validPwd && validPwdFormat && validConfirmPwd && validPwdMatch);
     }
 
-    private void addUserAndRedirect() {
-        Log.e(TAG, "--> Start addUserAndRedirect");
 
+    // Add user to database and redirect to login page
+    private void addUserAndRedirect() {
         String fnameStr = fname.getText().toString();
         String lnameStr = lname.getText().toString();
         String usernameStr = username.getText().toString();
         String emailStr = email.getText().toString();
         String pwdStr = pwdConfirm.getText().toString();
 
+        // Create new user with all the input data
         User user = new User(emailStr, usernameStr, pwdStr, fnameStr, lnameStr);
-        Log.e(TAG, "-->New user: " + user);
 
+        // Add the new user to the database
         dbHandler().addUserHandler(user);
+
+        // Display pop up message after user has been added to database
         Toast.makeText(RegistrationActivity.this, "Registered Successfully!", Toast.LENGTH_LONG).show();
 
-        //Redirect to login page
-        int timeout = 1500; // make the activity visible for 1.5 seconds
+        // Visible for 1.2 seconds
+        int timeout = 1200;
 
+        // This makes the current activity visible for the amount of "timeout" before redirecting
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
 
             @Override
             public void run() {
                 finish();
+
+                // Redirect to login page
                 Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
@@ -262,6 +287,7 @@ public class RegistrationActivity extends AppCompatActivity {
 
     }
 
+    // Hids the keyboard
     public void hideKeyboard(View view) {
         InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
