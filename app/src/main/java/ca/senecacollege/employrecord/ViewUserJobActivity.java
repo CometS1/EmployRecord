fd3b8ca4f5d8dedfef5062cdfa3d1ef61312106f
Shawn <sppang1@yahoo.com>
2019-04-01 17:04:29 -0400

    Added html usage to job board view job page

--- app/src/main/java/ca/senecacollege/employrecord/ViewUserJobActivity.java
+++ app/src/main/java/ca/senecacollege/employrecord/ViewUserJobActivity.java
@@ -3,6 +3,7 @@ package ca.senecacollege.employrecord;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -75,7 +76,7 @@ public class ViewUserJobActivity extends AppCompatActivity {
         String descriptionToken = job.getDescription();
         //descriptionToken = removeHtml(descriptionToken);
         TextView descriptionInfo = (TextView) findViewById(R.id.textViewDescription);
-        descriptionInfo.setText(descriptionToken);
+        descriptionInfo.setText(Html.fromHtml(descriptionToken));
 
         String creationDateToken = job.getPost_deadline();
         TextView creationDateInfo = (TextView) findViewById(R.id.textViewCreationDate);
